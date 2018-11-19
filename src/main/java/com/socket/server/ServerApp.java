073c2d0cbfa24b6bd2a36233a90eca6cf182ae30
Jiaqi Qin <frank.qjq@outlook.com>
2018-11-19 01:09:11 -0800

    1. add user entity 2. add broadcast 3. add interface for load unread messages and offline detection

--- src/main/java/com/socket/server/ServerApp.java
+++ src/main/java/com/socket/server/ServerApp.java
@@ -3,22 +3,39 @@ package com.socket.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServerApp {
+
+    private static ServerSocket serverSocket = null;
+    private static Socket clientSocket = null;
+    private static final int DEFAULT_PORT = 9091;
+    static List<ClientThread> onlineClientThreads;
+    static UserDao userDao = new UserDao();
 
     public static void main(String[] args) {
         ServerSocket serverSocket = null;
         try {
-            serverSocket = new ServerSocket(9091);
-            System.out.println("Server started, listening on 9091");
-            while(true){
-                Socket clientSocket = serverSocket.accept();
-                ConnectionThread notificationThread = new ConnectionThread(clientSocket);
-                notificationThread.start();
-            }
+            serverSocket = new ServerSocket(DEFAULT_PORT);
+            System.out.println("Server started, listening on " + DEFAULT_PORT);
+            onlineClientThreads = new ArrayList<>();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        while (true) {
+            try {
+                Socket clientSocket = serverSocket.accept();
+                ClientThread clientThread = new ClientThread(clientSocket);
+                onlineClientThreads.add(clientThread);
+                clientThread.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
+
 
 }
